e03cde6d96212b3eedbfbb538370b58cede457c1
Nick Ducker <nick_ducker@live.com>
2020-06-29 13:49:16 +0930

    update users to resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_041631) do
+ActiveRecord::Schema.define(version: 2020_06_29_041225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_06_23_041631) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
   end
 
 end
