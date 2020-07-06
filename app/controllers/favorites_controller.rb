bc33c7d17e598721b2635517b3cadcd67c5ee89d
Nick Ducker <nick_ducker@live.com>
2020-07-06 14:16:16 +0930

    add favoriting functionality to workouts

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,20 @@
 class FavoritesController < ApplicationController
+
+  def add
+    FavoriteWorkout.create(favorite_params)
+    redirect_to workouts_path
+  end
+
+  def remove
+    fav = FavoriteWorkout.find_by(user_id: favorite_params[:user_id], workout_id: favorite_params[:workout_id])
+    fav.destroy
+    redirect_to workouts_path
+  end
+
+private
+
+  def favorite_params
+    params.require(:favparams).permit(:user_id, :workout_id)
+  end
+
 end
